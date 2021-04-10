5c4fdc603eec2c17eda36a2ac873f651dfff6089
Denis Lafontant <icebox827@gmail.com>
2021-04-10 19:20:23 -0400

    Add all test for calculator

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,17 @@
 const calculator = require("../code/calculator");
 
 it('should add number', () => {
-  expect(2, 2).toBe(4)
+  expect(calculator.add(2, 2)).toBe(4);
+});
+
+it('should substract number', () => {
+  expect(calculator.sub(4, 2)).toBe(2);
+});
+
+it('should multiply number', () => {
+  expect(calculator.mul(3, 3)).toBe(9);
+});
+
+it('should divide number', () => {
+  expect(calculator.div(10, 2)).toBe(5);
 });
